709e5d960e8107560886ef0b05d719fb4fa932ce
Cristhian Roncancio <69442529+Cdavid111@users.noreply.github.com>
2021-11-29 14:20:01 -0500

    CRUD Entidad User

--- src/endpoints/user.js
+++ src/endpoints/user.js
@@ -1,10 +1,31 @@
 const router = require('express').Router()
 const userService = require('../services/user')
 
+//Create User
 router.post('/', async (req,res)=> {
     
     let userCreated = await userService.createUser(req.body)
     return res.status(201).json({userCreated})
+})
+//get all users
+router.get('/',async (req,res) => {
+    let users = await userService.getUsers()
+    return res.status(200).json({users})
+})
+//get only one user
+router.get('/:id',async (req,res) => {
+    let user = await userService.getUserById(req.params.id)
+    return res.status(200).json({user})
+})
+//update an user
+router.put('/:id',async (req,res) =>{
+    let new_user = await userService.updateUser(userId = req.params.id , user= req.body)
+    return res.status(200).json({new_user})
+})
+//Delete an User
+router.delete('/:id', async (req,res) => {
+    await userService.deleteUser(req.params.id)
+    return res.status(204).json({})
 })
 
 module.exports = router;
--- src/services/user.js
+++ src/services/user.js
@@ -1,10 +1,34 @@
 const User = require('../models/user');
 
+//Create User
 createUser = async (user) => {
     let userInstance = new User(user)
     userInstance.save()
 }
+//get all users
+getUsers = async () => {
+    users = await User.find({})
+    return users
+}
+//get only one user
+getUserById = async (userId) => {
+    user = await User.findById(userId).exec()
+    return user 
+}
+//update an user
+updateUser = async (userId,user) => {
+    new_user = await User.findByIdAndUpdate(userId,user,{new: true})
+    return new_user
+}
+//Delete an user
+deleteUser = async (userId) => {
+    return await User.findByIdAndDelete(userId)
+}
 
 module.exports = {
-    createUser
+    createUser,
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser
 }
