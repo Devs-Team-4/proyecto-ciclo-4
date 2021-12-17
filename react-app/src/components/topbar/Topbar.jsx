3ee62aa7c24472873af591ef5f9db7c3f0879881
Cristhian Roncancio <69442529+Cdavid111@users.noreply.github.com>
2021-12-17 03:09:18 -0500

    Creando topbar

---
+++ react-app/src/components/topbar/Topbar.jsx
@@ -0,0 +1,20 @@
+ import React from 'react'
+ import './topbar.css'
+ 
+ export default function  Topbar() {
+     return (
+         <div>
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">Developers_MisionTic</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIcons">
+                         
+                    </div>
+                </div>
+            </div>
+         </div>
+     )
+ }
+ 
