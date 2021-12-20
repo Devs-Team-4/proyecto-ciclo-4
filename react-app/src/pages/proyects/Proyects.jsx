a12e9000b37fb9f9a400d51a73b989b69c2c898f
Cristhian Roncancio <69442529+Cdavid111@users.noreply.github.com>
2021-12-20 13:55:38 -0500

    Tarjetas proyectos
    Se crearon las tarjetas en html5  y css3 en la pagina Proyectos, que permiten mostrar la información de los proyectos registrados

--- react-app/src/pages/proyects/Proyects.jsx
+++ react-app/src/pages/proyects/Proyects.jsx
@@ -4,19 +4,34 @@ export default function proyects() {
     return (
         <div className="proyects">
             <div>
-                <h1>Proyectos</h1>
-                <div className="container">
+                <h1>Proyectos Registrados</h1>
+                <div className="container1">
                     <div className="card">
-                        <img src="" alt="" />
-                        <h4>Proyecto 1</h4>
-                        <p>Lorem ipsum, dolor, sit amet consectetur adipisicing elit. Iste fuga quisquam cum optio 
-                        veniam</p>
-                        <h4>Topic : Astronomia</h4>
-                        <h4>owner : David Camilo</h4>
-                        <a href="#">Leer más...</a>
+                        <div className="img" />
+                        <h4 className="nameproject">Proyecto 1</h4>
+                        <p className="parrafo">Lorem ipsum, dolor sit amet consectetur dolor sit amet consectetur dolor sit</p>
+                        <p><span>Owner: </span>  Cristhian</p>
+                        <p><span>Topic: </span>  Astronomia</p>
+                        <a href="#">Leer mas</a>
+                    </div>
+                    <div className="card">
+                        <div className="img img2" />
+                        <h4 className="nameproject">Proyecto 2</h4>
+                        <p className="parrafo">Lorem ipsum, dolor sit amet consectetur dolor sit amet consectetur dolor sit</p>
+                        <p><span>Owner: </span>  Cristhian</p>
+                        <p><span>Topic: </span>  Astronomia</p>
+                        <a href="#">Leer mas</a>
+                    </div>
+                    <div className="card">
+                        <div className="img img3" />
+                        <h4 className="nameproject">Proyecto 3</h4>
+                        <p className="parrafo">Lorem ipsum, dolor sit amet consectetur dolor sit amet consectetur dolor sit</p>
+                        <p><span>Owner: </span>  Cristhian</p>
+                        <p><span>Topic: </span>  Astronomia</p>
+                        <a href="#">Leer mas</a>
                     </div>
                 </div>
-            </div
+            </div>
         </div>
     )
 }
